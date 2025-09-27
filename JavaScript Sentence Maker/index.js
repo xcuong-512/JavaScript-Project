61551f12e25ed6ff804b205e02e269acae440db4
Xuan Cuong <xcuong286@gmail.com>
2025-09-28 00:27:53 +0700

    Sentence Maker

---
+++ JavaScript Sentence Maker/index.js
@@ -0,0 +1,19 @@
+let adjective = `tiny`,
+    noun = `dragon`,
+    noun2 = `marshmallows`,
+    place = `volcano`,
+    adjective2 = `gigantic`,
+    verb = `sneezing`;
+
+let firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+console.log(`First story: ${firstStory}`);
+
+adjective = `curious`,
+noun = `kitten`,
+noun2 = `strawberries`,
+place = `floating castle`,
+adjective2 = `sparkling`,
+verb = `laughing`;
+
+let secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+console.log(`Second story: ${secondStory}`);
